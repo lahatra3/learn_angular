069a238a542a2b7c8a7f145ee37b02ac250a8840
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-07-06 22:51:21 +0300

    mise place du projet...

--- awesome-components/src/app/app.module.ts
+++ awesome-components/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { HeaderComponent } from './core/components/header/header.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- awesome-components/src/app/social-media/social-media.module.ts
+++ awesome-components/src/app/social-media/social-media.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { SocialMediaRoutingModule } from './social-media-routing.module';
 
-
 @NgModule({
   declarations: [],
   imports: [
