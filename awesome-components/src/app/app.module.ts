d9a7f90a250087c73ca15a58dfeb2f3e482aaec8
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-07-07 21:19:33 +0300

    [ADD] mat-toobar...

--- awesome-components/src/app/app.module.ts
+++ awesome-components/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { HeaderComponent } from './core/components/header/header.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
